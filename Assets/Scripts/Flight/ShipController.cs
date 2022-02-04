8e2c7ab17cce18496657e0fb0930460158e5739c
Sean Walton <sean@pillbug.zone>
2022-02-04 11:17:53 +0000

    Implemented steering

--- Assets/Scripts/Flight/ShipController.cs
+++ Assets/Scripts/Flight/ShipController.cs
@@ -7,18 +7,25 @@ public class ShipController : MonoBehaviour
 {
     [SerializeField] private Transform myForward;
     [SerializeField] private float mySpeed;
-    [SerializeField] private float myRotationSpeed;
+    [SerializeField] private float maxRotationSpeed;
+    [SerializeField] private Transform myTarget;
 
     private Rigidbody2D rb2D;
+    private float currentRotationSpeed = 0f;
+    private float signedAngle;
+    private Vector2 toTarget = new Vector2();
+    private Transform myTransform;
 
     private void Awake()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        myTransform = transform;
     }
 
     private void FixedUpdate()
     {
         UpdateVelocity();
+        
         UpdateRotation();
     }
 
@@ -29,7 +36,15 @@ public class ShipController : MonoBehaviour
 
     private void UpdateRotation()
     {
-        rb2D.angularVelocity = myRotationSpeed;
+        toTarget.x = myTarget.position.x - myTransform.position.x;
+        toTarget.y = myTarget.position.y - myTransform.position.y;
+
+        signedAngle = Vector2.SignedAngle(myForward.up, toTarget);
+
+        currentRotationSpeed = signedAngle / 180f;
+        
+        rb2D.angularVelocity = currentRotationSpeed * maxRotationSpeed;
     }
 
+    
 }
